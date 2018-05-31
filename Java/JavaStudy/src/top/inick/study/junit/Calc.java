405217fecc29f4d6b6463b59256a4f899362876e
iNick <228919961@qq.com>
2018-05-31 11:18:21 +0800

    了解 JUunit

---
+++ Java/JavaStudy/src/top/inick/study/junit/Calc.java
@@ -0,0 +1,13 @@
+package top.inick.study.junit;
+
+public class Calc {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public double div(double a, int b) {
+		return a / b;
+	}
+
+}
---
+++ Java/JavaStudy/src/top/inick/study/junit/TestCalc.java
@@ -0,0 +1,16 @@
+package top.inick.study.junit;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+//注:测试方法要求:不能有返回值,不能有参数
+public class TestCalc {
+
+	@Test
+	public void test1() {
+		Calc c = new Calc();
+		Assert.assertEquals(3, c.div(10, 3), 0.4);
+	}
+	
+}
