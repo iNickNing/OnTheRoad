fbfb365a4d508b03d749b3d2936afad5a2a86906
iNick <228919961@qq.com>
2018-07-14 16:42:42 +0800

    记录

--- Java/WebPool/src/top/inick/datasource/MyConnection.java
+++ Java/WebPool/src/top/inick/datasource/MyConnection.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Executor;
 
-//1、编写一个类，实现与被包装类相同的接口。（具备相同的行为）
+//1、编写一个类，实现与被包装类相同的接口。（具备相同的行为） 
 //2、定义一个被包装类类型的变量。
 //3、定义构造方法，把被包装类的对象注入，给被包装类变量赋值。
 //4、对于不需要改写的方法，调用原有的方法。
